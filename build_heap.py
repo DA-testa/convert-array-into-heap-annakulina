a848e14f16830492ddddd198200db8b35e93afdf
annakulina <anna.kulina2@gmail.com>
2023-03-18 18:40:31 +0200

    Anna Kūliņa 14.gr.

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,49 @@
 # python3
+# Anna Kūliņa 14.gr.
 
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
+    gar = len(data)
+    for i in range(gar//2 - 1, -1, -1):
+        a = i
+        while 2*a+1 < gar:
+            kr = 2*a+1
+            if kr+1 < gar and data[kr+1] < data[kr]:
+                kr = kr+1
+            if data[a] >= data[kr]:
+                swaps.append((a, kr))
+                data[a], data[kr] = data[kr], data[a]    
+                a = kr
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+
+    text = input("Ievadiet F testiem vai I manuālai ievadei: ")
+    if 'F' in text:
+        fails = input("Ievadiet faila nosaukumu: ")
+        if fails:
+            try:
+                with open("./test/" + fails) as fails1:
+                    n = int(fails1.readline())
+                    data = list(map(int, fails1.readline().split()))
+            except FileNotFoundError:
+                print("Fails nav atrasts")
+                return
+            
+    elif 'I' in text:
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        swaps = build_heap(data)
+
+    else:
+        print("Nepareiza komanda")
+        return
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
